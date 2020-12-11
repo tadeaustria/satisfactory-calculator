fe18187c669cb3c570a77ff343766df378f0ca7b
Daniel Gogl <tade@gmx.at>
2020-12-11 10:27:02 +0100

    Include FICSMASFix "Alternate: Bolted Iron Plate" Receipt

--- factory.js
+++ factory.js
@@ -21,7 +21,7 @@ import { renderTotals } from "./visualize.js"
 
 const DEFAULT_ITEM_KEY = "supercomputer"
 
-let minerCategories = new Set(["mineral", "oil", "water"])
+let minerCategories = new Set(["mineral", "oil", "water", "gift-tree"])
 
 export let resourcePurities = [
     {key: "0", name: "Impure", factor: half},
